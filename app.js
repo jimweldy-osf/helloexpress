0ac63e8cc30e488236739dc917fb7dcb06d54cd5
Mohammad Azam <azamsharp@gmail.com>
2023-03-27 10:37:21 -0500

    added mustache

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mustacheExpress = require('mustache-express')
 const app = express() 
 
+// setting up Express to use Mustache Express as template pages 
+app.engine('mustache', mustacheExpress())
+// the pages are located in views directory
+app.set('views', './views')
+// extension will be .mustache
+app.set('view engine', 'mustache')
+
 app.get('/', (req, res) => {
-    res.send('HELLO')
+    res.render('index')
 })
 
 app.listen(8080, () => {
